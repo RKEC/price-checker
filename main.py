bd449d8b6b375d3916a2e70e359a6d70aa46262b
RKEC <r.kieran.collins@gmail.com>
2024-03-23 21:54:11 +0000

    Version 2: Gets updated price every hour

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import logging
 import json
 import os
 import smtplib
+from time import sleep
 from dotenv import load_dotenv
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import random
 
 def user_input():
     while True:
@@ -20,41 +22,50 @@ def user_input():
     return url, price
 
 def scrape_data(url, headers):
-    try:
-        page = requests.get(url=url, headers=headers)
-        page.raise_for_status()
-        raw = BeautifulSoup(page.content, 'html.parser')
-        return raw
-    except requests.exceptions.MissingSchema:
-        logging.error("Invalid URL: No scheme supplied. Please make sure to include http:// or https:// in the URL.")
-    except requests.exceptions.ConnectionError:
-        logging.error("Can't connect to the server. Please check your internet connection.")
-
-def gather_data(url, price):
-    headers = {'User-Agent': 'Safari/605.1.15'}
-    pretty = scrape_data(url, headers)
-    if pretty:
+    while True:
         try:
-            title_element = pretty.find(id="productTitle")
-            price_element = pretty.find(id="priceValue")
+            page = requests.get(url=url, headers=headers, timeout=30)
+
+            raw = BeautifulSoup(page.content, 'html.parser')
+
+            title_element = raw.find(id="productTitle")
+            price_element = raw.find(id="priceValue")
+
+            return title_element, price_element
+        except requests.exceptions.MissingSchema:
+            logging.error("Invalid URL: No scheme supplied. Please make sure to include http:// or https:// in the URL.")
+        except requests.exceptions.ConnectionError:
+            logging.error("Can't connect to the server. Please check your internet connection.")
+
+def gather_data(url, price, scrape=True):
+    headers = {'User-Agent': 'Safari/605.1.15'}
+    previous_price = 1000000.00
+
+    while scrape:    
+        title_element, price_element = scrape_data(url, headers)
+        try:
             if title_element and price_element:
                 title = title_element.get_text().strip().split(',')[0]
-                current_price = price_element.get('value')
+                current_price = float(price_element.get('value'))
                 data = {
                     "url": url,
                     "title": title,
-                    "price": float(current_price),
-                    "target_price": float(price)
+                    "price": current_price,
+                    "target_price": price,
+                    "previous_price": previous_price
                 }
-                print(json.dumps(data, indent=4))
-                if data['price'] <= price:
-                    send_email(os.getenv('TO_EMAIL'), data)
+                if current_price < previous_price:
+                    if current_price <= price:
+                        send_email(os.getenv('TO_EMAIL'), data)
+                previous_price = current_price
+                sleep(3600)
             else:
                 logging.error("Couldn't find necessary data on the page.")
         except Exception as e:
             logging.error(f"Error occurred while parsing data: {e}")
     else:
         logging.error("No data received from the server.")
+
 
 def setup_email_server():
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -115,16 +126,17 @@ def send_email(to_email, data):
     </body>
     </html>
     """
-    
-    message.attach(MIMEText(body, 'html'))
 
+    message.attach(MIMEText(body, 'html'))
     text = message.as_string()
     server.sendmail(os.getenv('SENDER_EMAIL'), to_email, text)
     server.quit()
     print("Email sent!")
 
-
-if __name__ == '__main__':
+def main():
     load_dotenv()
     url, price = user_input()
     gather_data(url, price)
+
+if __name__ == '__main__':
+    main()
